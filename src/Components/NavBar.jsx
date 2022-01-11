d307d4b939b515d0dd4b90d2b435c569e86cca2f
Pratham <agrawalpratham2002@gmail.com>
2022-01-11 14:40:25 +0530

    Navbar Components

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,13 @@
+import "./navbar.module.css";
+
+
+export default function NavBar(){
+
+    return(
+        <>
+        <div className="top-bar"> 
+            <img src="q"></img>
+        </div>
+        </>
+    )
+}
