2fc418c3335b4e294ffe56edc284303223c93960
Vandna <vandana100718@gmail.com>
2022-01-11 18:11:31 +0530

    signup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import { Home } from './Pages/Home';
+import {SignUp } from './Pages/SignUp';
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <SignUp/>
     </div>
   );
 }
--- src/Pages/home.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Home = () => {
-    return (
-        <div>
-            <h1>This is home page</h1>
-        </div>
-    )
-}
